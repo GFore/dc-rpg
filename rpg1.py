722f22ed9cfe682ce7651f047831ed4701638dbc
Greg Foreman <gfore@mindspring.com>
2018-09-28 14:10:56 -0400

    adds choice of enemy; simplifies class defs

--- rpg1.py
+++ rpg1.py
@@ -7,10 +7,18 @@ In this simple RPG game, the hero fights the goblin. He has the options to:
 
 """
 
-# Step 7: The alive methods on Hero and Goblin should be identical. Move it into Character, and remove them from Hero and Goblin - now they can simply inherit it from Character.
-# Step 7: Bonus Challenge: The methods attack and print_status method in Hero and Goblin look almost identical, but not quite. Is it possible to move them into the Character class as well? Give it a try
+# Step 8: Bonus Challenge 2: Create a zombie character that cannot die and have it fight the hero instead of the goblin.
 
 def main():
+
+    def menu_choice(ename):
+        print()
+        print("What do you want to do?")
+        print("1. fight " + ename)
+        print("2. do nothing")
+        print("3. flee")
+        print("> ", end='')
+        return input()
 
     class Character:
         def __init__(self, name, health, power):
@@ -22,52 +30,48 @@ def main():
             return self.health > 0
 
         def attack(self, enemy):
-            enemy.health -= self.power
+            if enemy.name != 'zombie':
+                enemy.health -= self.power
             if self.name == 'hero':
-                print("You do %d damage to the goblin." % self.power)
+                print("You do %d damage to the %s." % (self.power if enemy.name != 'zombie' else 0, enemy.name))
             else:
-                print("The goblin does %d damage to you." % self.power)
+                print("The %s does %d damage to you." % (self.name, self.power))
 
         def print_status(self):
             if self.alive():
-                if self.name == 'hero':
-                    print("You have %d health and %d power." % (self.health, self.power))
-                else:
-                    print("The goblin has %d health and %d power." % (self.health, self.power))
+                whohas = "You have" if self.name == 'hero' else "The %s has" % (self.name)
+                print('%s %d health and %d power.' % (whohas, self.health, self.power))
             else:
-                if self.name == 'hero':
-                    print("You are dead.")
-                else:
-                    print("The goblin is dead.")
-
+                print("You are dead." if self.name == 'hero' else "The %s is dead." % (self.name))
 
     hero = Character('hero', 10, 5)
+    zombie = Character('zombie', 6, 2)
     goblin = Character('goblin', 6, 2)
+ 
+    try:
+        enemy_choice = int(input("Enemy? 1) Goblin or 2) Zombie: "))
+        if enemy_choice == 1:
+            enemy = goblin
+        else:
+            enemy = zombie
+    except:
+        enemy = goblin
 
-
-    while goblin.alive() and hero.alive():
+    while enemy.alive() and hero.alive():
         hero.print_status()
-        goblin.print_status()
-        print()
-        print("What do you want to do?")
-        print("1. fight goblin")
-        print("2. do nothing")
-        print("3. flee")
-        print("> ", end='')
-        user_input = input()
-        if user_input == "1":               # Hero attacks goblin
-            hero.attack(goblin)
-            if not goblin.alive(): goblin.print_status()
-        elif user_input == "2":
-            pass
+        enemy.print_status()
+        user_input = menu_choice(enemy.name)
+
+        if user_input == "1":               # Hero attacks
+            hero.attack(enemy)
+            if not enemy.alive(): enemy.print_status()
         elif user_input == "3":
             print("Goodbye.")
             break
-        else:
-            print("Invalid input %r" % user_input)
+        else: print("Invalid input %r" % (user_input))
 
-        if goblin.alive():               # Goblin attacks hero
-            goblin.attack(hero)
-            if not hero.alive(): hero.print_status()
+        if enemy.alive(): enemy.attack(hero)         # enemy attacks hero    
+        
+        if not hero.alive(): hero.print_status()
 
 main()
